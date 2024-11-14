010b853c968408d930c81a326f302d79b1def9a2
Rider7918 <ghostrider7918@gmail.com>
2024-11-14 17:15:52 +0530

    added login logout functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,21 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages and components
-import Home from './pages/Home'
-import Navbar from './components/Navbar';
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <div className='pages'>
+        <div className="pages">
           <Routes>
-            <Route 
-            path='/'
-            element={<Home/>}
-            > 
-
-            </Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
           </Routes>
         </div>
       </BrowserRouter>
